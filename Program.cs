98a649135d6ff557d8ee7c267f93c08d5b142721
GeneralGilmar <gaspar.general1987@gmail.com>
2023-06-05 13:35:38 +0100

    commiting

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankAccountException.Entities;
+using BankAccountException.Entities.Exception;
+using System.Globalization;
+
+
+
+try
+{
+    Console.WriteLine("Enter account data");
+
+    Console.Write("Number: ");
+    int number = int.Parse(Console.ReadLine());
+    Console.Write("Holder: ");
+    string holder = Console.ReadLine();
+    Console.Write("Initial balance: ");
+
+    double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Console.Write("Withdraw limit: ");
+
+    double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Account account = new Account(number, holder, balance, withdrawLimit);
+
+    Console.Write("Enter amount for withdraw: ");
+    double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    account.Withdraw(amount);
+
+    Console.WriteLine(account);
+}
+catch (DomainException e)
+{
+    Console.WriteLine("Withdraw error: " + e.Message);
+}
+catch (FormatException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
